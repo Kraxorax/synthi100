47ca54dbe3cb57bb79d320f3a710981a76caf3b5
Kraxorax <gmarkokralj@gmail.com>
2019-01-17 18:21:39 +0100

    matrix of knobs

--- src/Main.elm
+++ src/Main.elm
@@ -3,11 +3,15 @@ import Html
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
-
+import Array
+import Matrix exposing (Matrix, generate, toArray)
 
 
 init: () -> (Model, Cmd msg)
 init flags = ({ circleFill = "#0000ff"}, Cmd.none)
+
+table: Matrix (Svg msg)
+table = generate 8 7 (\x y -> knob10Svg (x * 50, y * 50) True (toFloat (x + y)))
 
 subs: Model -> Sub msg
 subs model = Sub.none
@@ -36,62 +40,56 @@ knobValueToAngle: Float -> Float
 knobValueToAngle x =
   30 + x * 30
 
-knob10Svg: Bool -> Float -> Svg msg
-knob10Svg active val =
+knob10Svg: (Int, Int) -> Bool -> Float -> Svg msg
+knob10Svg (kx, ky) active val =
   let 
     rotation = "rotate(" ++ String.fromFloat (knobValueToAngle val) ++ " 20 20)"
   in
-  g 
-  [ transform rotation
+  svg
+  [ x (String.fromInt kx)
+  , y (String.fromInt ky)
   ]
-  [ circle
-    [ cx "20"
-    , cy "20"
-    , r "20"
-    , fill "#333333"
+  [
+    svg
+    [ width "40"
+    , height "40"
+    , viewBox "0 0 40 40"
+    , transform rotation
     ]
-    []
-  , rect
-    [ x "18"
-    , y "20"
-    , rx "2"
-    , ry "2"
-    , width "4"
-    , height "20"
-    , fill "#999999"
-    ]
-    []
+      [
+        circle
+          [ cx "20"
+          , cy "20"
+          , r "20"
+          , fill "#333333"
+          ]
+          []
+        , rect
+          [ x "18"
+          , y "20"
+          , rx "2"
+          , ry "2"
+          , width "4"
+          , height "20"
+          , fill "#999999"
+          ]
+          []
+      ]
   ]
+
 
 
 view: Model -> Html.Html (Msg)
 view model =
+  let
+    knobs = table |> toArray |> Array.toList
+  in
+
   Html.div []
-    [ text "njaaaaghh"
-    , svg
+    [ svg
       [ width "1400"
       , height "2400"
       , viewBox "0 0 1400 2400"
       ]
-      [ rect
-          [ x "10"
-          , y "10"
-          , width "100"
-          , height "100"
-          , rx "15"
-          , ry "15"
-          , fill "#ff0000"
-          ]
-          []
-      , circle
-          [ cx "50"
-          , cy "50"
-          , r "50"
-          , fill model.circleFill
-          , onMouseOver OverCircle
-          , onMouseOut OutCircle
-          ]
-          []
-      , knob10Svg True 5.0
-      ]
+      knobs
     ]
